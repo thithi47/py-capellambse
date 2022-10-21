e81ed199ba0b2b02c8b4f21c45a5cc5a8d88d01e
Martin Lehmann <martin.lehmann@deutschebahn.com>
2022-10-21 14:38:46 +0200

    fix(loader): Don't return Nones from `follow_links`
    `follow_links` used to indicate broken links by placing `None` at its
    position in the returned list. However, almost none of the call sites
    were actually prepared to handle the `None` cases; and the one site that
    was simply ignored them. As the previous behavior has led to crashes
    with very unhelpful stack traces, the method has been "fixed" to raise
    the original `KeyError` from accessing the broken link, unless the new
    keyword-only argument `ignore_broken` is set to `True` in the
    `follow_links` call. The call site that was prepared to ignore broken
    links sets this flag, all others leave it at its default of `False`.

--- capellambse/loader/core.py
+++ capellambse/loader/core.py
@@ -1071,7 +1071,9 @@ class MelodyLoader:
         self,
         from_element: etree._Element | None,
         links: str,
-    ) -> list[etree._Element | None]:
+        *,
+        ignore_broken: bool = False,
+    ) -> list[etree._Element]:
         """Follow multiple links and return all results as list.
 
         The format for an individual link is the same as accepted by
@@ -1088,9 +1090,14 @@ class MelodyLoader:
         links
             A string containing space-separated links as described in
             :meth:`follow_link`.
+        ignore_broken
+            Ignore broken references instead of raising a KeyError.
 
         Raises
         ------
+        KeyError
+            If any link points to a non-existing target. Can be
+            suppressed with ``ignore_broken``.
         ValueError
             If any link is malformed.
         RuntimeError
@@ -1111,7 +1118,8 @@ class MelodyLoader:
             try:
                 targets.append(self.follow_link(from_element, part))
             except KeyError:
-                targets.append(None)
+                if not ignore_broken:
+                    raise
         return targets
 
     def _find_fragment(
